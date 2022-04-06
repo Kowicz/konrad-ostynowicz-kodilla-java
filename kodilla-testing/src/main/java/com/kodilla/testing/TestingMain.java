5e2ac7916e10cd2cdff4c430676b333436bbc57f
Konrad Ostynowicz <konrad.ostynowicz@gmail.com>
2022-04-06 12:04:57 +0200

    6.2 Added SimpleCalculator and SimpleUser classes with adequate tests in TestingMain.

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,47 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.SimpleCalculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.");
+
+        //testing simpleUser
+        System.out.println("\n--TESTING simpleUser--\n");
+        System.out.println("TESTING constructor");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("OK - constructor test passed");
+        } else {
+            System.out.println("NOT OK - constructor test failed");
+        }
+
+
+        //testing simpleCalculator
+        System.out.println("\n--TESTING simpleCalculator--\n");
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+
+        //adding test
+        System.out.println("TESTING adding");
+        int addingResult = simpleCalculator.add(7, 24);
+
+        if (addingResult == 7 + 24) {
+            System.out.println("OK - addition test passed");
+        } else {
+            System.out.println("NOT OK - addition test failed");
+        }
+
+        //subtracting test
+        System.out.println("\nTESTING subtracting");
+        int subtractingResult = simpleCalculator.subtract(49, 12);
+
+        if (subtractingResult == 49 - 12) {
+            System.out.println("OK - subtraction test OK");
+        } else {
+            System.out.println("NOT OK - subtraction test failed");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/SimpleCalculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.calculator;
+
+public class SimpleCalculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
